4b5cefb3152d9b98795f2756f0c34a99a8a92422
mistheo <th.mistral@gmail.com>
2025-06-30 16:17:43 +0200

    Ajout des énumérations Permission, UserRole et FieldVisibility

---
+++ fastforge/app/enums/permissions.py
@@ -0,0 +1,29 @@
+"""
+Permissions, User Roles, FieldVisibility enumerations
+"""
+
+from enum import Enum
+
+
+class Permission(str, Enum):
+    """Enumération des permissions"""
+    PUBLIC = "public"
+    USER = "user"
+    ADMIN = "admin"
+    SUPER_ADMIN = "super_admin"
+
+
+class UserRole(str, Enum):
+    """Enumération des rôles utilisateur"""
+    USER = "USER"
+    ADMIN = "ADMIN"
+    SUPER_ADMIN = "SUPER_ADMIN"
+
+
+class FieldVisibility(str, Enum):
+    """Enumération de la visibilité des champs"""
+    PUBLIC = "public"
+    USERS = "users"
+    USER = "user"
+    ADMIN = "admin"
+    SUPER_ADMIN = "super_admin"
