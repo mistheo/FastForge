0fd36b726a2e8a3d5d9531bc7dc0bdfe0161a0ae
mistheo <th.mistral@gmail.com>
2025-06-30 16:20:14 +0200

    Ajout de l'énumération HookType pour gérer les types de hooks

---
+++ fastforge/app/enums/hooks.py
@@ -0,0 +1,15 @@
+"""
+Hooks enumeration
+"""
+
+from enum import Enum
+
+
+class HookType(str, Enum):
+    """Enumération des types de hooks"""
+    BEFORE_REQUEST = "before_request"
+    AFTER_REQUEST = "after_request"
+    BEFORE_AUTH = "before_auth"
+    AFTER_AUTH = "after_auth"
+    BEFORE_RESPONSE = "before_response"
+    ON_ERROR = "on_error"
