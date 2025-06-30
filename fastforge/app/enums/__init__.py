c4649ada1f58c424a622765964d0e9f2b24ca881
mistheo <th.mistral@gmail.com>
2025-06-30 16:20:18 +0200

    Ajout de l'énumération des méthodes HTTP et mise à jour du module d'énumérations

---
+++ fastforge/app/enums/__init__.py
@@ -0,0 +1,22 @@
+"""
+Enums module - Expose toutes les énumérations
+"""
+
+from .http import HTTPMethod
+from .crud import CRUDOperation
+from .permissions import Permission, UserRole, FieldVisibility
+from .cache import CacheType, CacheDuration, RateWindow, RateLimit
+from .hooks import HookType
+
+__all__ = [
+    'HTTPMethod',
+    'CRUDOperation', 
+    'Permission',
+    'UserRole',
+    'FieldVisibility',
+    'CacheType',
+    'CacheDuration',
+    'RateWindow',
+    'RateLimit',
+    'HookType'
+]
