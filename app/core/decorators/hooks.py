78b6940cca1b652bf6c517d5e9dd1a1faf78f77c
mistheo <th.mistral@gmail.com>
2025-07-02 17:27:17 +0200

    Ajout de la classe HooksDecorator et du décorateur hooks pour la gestion des hooks dans le système

---
+++ app/core/decorators/hooks.py
@@ -0,0 +1,31 @@
+"""
+@hooks - Système de hooks
+"""
+
+from typing import Dict, List, Callable, Any
+from ...enums.hooks import HookType
+
+
+class HooksDecorator:
+    """Décorateur pour le système de hooks"""
+    
+    def __init__(self, hooks_config: Dict[str, List[str]]):
+        self.hooks_config = hooks_config
+    
+    def __call__(self, func: Callable) -> Callable:
+        """Applique le décorateur à la fonction"""
+        pass
+    
+    def execute_hooks(self, hook_type: HookType, **kwargs) -> Any:
+        """Exécute les hooks d'un type donné"""
+        pass
+
+
+def hooks(**hooks_config) -> Callable:
+    """Décorateur pour le système de hooks"""
+    
+    def decorator(func: Callable) -> Callable:
+        func._fastforge_hooks_config = hooks_config
+        return func
+    
+    return decorator
