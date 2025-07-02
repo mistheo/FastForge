72ade23bc9047fedcddd575d6d4b3a386976973f
mistheo <th.mistral@gmail.com>
2025-07-02 17:04:30 +0200

    Ajout du module __init__.py pour exposer BaseSchema et UserSchema

---
+++ app/schemas/__init__.py
@@ -0,0 +1,11 @@
+"""
+Schemas module - Expose BaseSchema et tous les schémas
+"""
+
+from .base import BaseSchema
+from .user import UserSchema
+
+__all__ = [
+    'BaseSchema',
+    'UserSchema'
+]
