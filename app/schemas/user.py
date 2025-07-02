28f9b5e9318dad334e619e2bee3f2b18a4340fde
mistheo <th.mistral@gmail.com>
2025-07-02 17:03:54 +0200

    Ajout de la classe UserSchema pour structurer les données utilisateur au niveau de l'API

---
+++ app/schemas/user.py
@@ -0,0 +1,18 @@
+"""
+UserSchema + LoginForm
+"""
+
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+from .base import BaseSchema
+from ..enums.permissions import UserRole
+
+
+class UserSchema(BaseSchema):
+    """Schéma pour les données utilisateur"""
+    
+    username: str
+    email: EmailStr
+    role: UserRole
+    last_login: Optional[datetime] = None
