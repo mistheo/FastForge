cae071c38916671eab9cb82a0d137cb5dfe9763d
mistheo <th.mistral@gmail.com>
2025-07-03 10:44:05 +0200

    Ajout du module middleware avec l'exposition des middlewares CORSMiddleware, LoggingMiddleware et ErrorHandlerMiddleware

---
+++ app/middleware/__init__.py
@@ -0,0 +1,13 @@
+"""
+Middleware module - Expose tous les middlewares
+"""
+
+from .cors import CORSMiddleware
+from .logging import LoggingMiddleware
+from .error_handler import ErrorHandlerMiddleware
+
+__all__ = [
+    'CORSMiddleware',
+    'LoggingMiddleware',
+    'ErrorHandlerMiddleware'
+]
