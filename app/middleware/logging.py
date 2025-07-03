9566950442153090a863c74350360fe86c7d1718
mistheo <th.mistral@gmail.com>
2025-07-03 10:43:59 +0200

    Ajout du middleware LoggingMiddleware pour le logging des requêtes dans FastAPI

---
+++ app/middleware/logging.py
@@ -0,0 +1,30 @@
+"""
+Middleware de logging des requêtes
+"""
+
+import time
+import logging
+from typing import Callable
+from fastapi import Request, Response
+
+
+logger = logging.getLogger(__name__)
+
+
+class LoggingMiddleware:
+    """Middleware de logging pour FastAPI"""
+    
+    def __init__(self, app):
+        self.app = app
+    
+    async def __call__(self, request: Request, call_next: Callable) -> Response:
+        """Log les requêtes et réponses"""
+        pass
+    
+    def log_request(self, request: Request):
+        """Log les détails de la requête"""
+        pass
+    
+    def log_response(self, request: Request, response: Response, process_time: float):
+        """Log les détails de la réponse"""
+        pass
