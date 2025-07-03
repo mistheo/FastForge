554993a16739ce156839d1d69697cbc779c63b4c
mistheo <th.mistral@gmail.com>
2025-07-03 10:37:53 +0200

    Ajout de la classe DatabaseCache pour la gestion du cache en base de données avec MongoDB

---
+++ app/core/cache/database.py
@@ -0,0 +1,35 @@
+"""
+Implémentation cache MongoDB
+"""
+
+from typing import Any
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class DatabaseCache:
+    """Cache en base de données pour la production"""
+    
+    def __init__(self, db_client: AsyncIOMotorClient, db_name: str = "fastforge_cache"):
+        self.db_client = db_client
+        self.db_name = db_name
+        self.collection_name = "cache"
+    
+    async def get(self, key: str) -> Any:
+        """Récupère une valeur du cache database"""
+        pass
+    
+    async def set(self, key: str, value: Any, duration: int = 300):
+        """Stocke une valeur dans le cache database"""
+        pass
+    
+    async def delete(self, key: str):
+        """Supprime une clé du cache database"""
+        pass
+    
+    async def clear(self):
+        """Vide le cache database"""
+        pass
+    
+    async def _cleanup_expired(self):
+        """Nettoie les entrées expirées"""
+        pass
